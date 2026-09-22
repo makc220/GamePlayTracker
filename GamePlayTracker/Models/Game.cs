be83945a93b5c77cdf947a8c8125f60382b8cc84
Бондарчук Максим <bondarcukm086@gmail.com>
2026-09-22 10:44:13 +0300

    ЛР 2: модель предметної області

---
+++ GamePlayTracker/Models/Game.cs
@@ -0,0 +1,18 @@
+namespace GamePlayTracker.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = "";
+
+    public string Genre { get; set; } = "";
+
+    public string Developer { get; set; } = "";
+
+    public DateOnly ReleaseDate { get; set; }
+
+    public int ReleaseYear { get; set; }
+
+    public string? Description { get; set; }
+}
